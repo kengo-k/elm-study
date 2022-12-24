81d3cfb5a51bc08f22ade48b49b345415558567e
kengo-k <kurobanekengo@gmail.com>
2022-12-24 09:36:37 +0900

    02: モジュールに関するコメント追記

--- 02-basic_html/src/Main.elm
+++ 02-basic_html/src/Main.elm
@@ -1,3 +1,6 @@
+-- モジュールMainを定義。モジュール名はファイル名と一致する(Main.elm)
+-- メインモジュールはmainという名前の関数をエクスポートする必要がある
+-- メインモジュール名はMainである必要はない
 module Main exposing(main)
 
 -- HtmlモジュールからHTML型とdiv関数, text関数をインポートする
