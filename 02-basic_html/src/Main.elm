ab7e27dd3d4694a04a0985f862ae93280fcb768a
kengo-k <kurobanekengo@gmail.com>
2022-12-24 11:09:35 +0900

    フォーマット適用 close #4

--- 02-basic_html/src/Main.elm
+++ 02-basic_html/src/Main.elm
@@ -1,13 +1,17 @@
 -- モジュールMainを定義。モジュール名はファイル名と一致する(Main.elm)
 -- メインモジュールはmainという名前の関数をエクスポートする必要がある
 -- メインモジュール名はMainである必要はない
-module Main exposing(main)
+
+
+module Main exposing (main)
 
 -- HtmlモジュールからHTML型とdiv関数, text関数をインポートする
+
 import Html exposing (Html, div, text)
-import Html.Attributes exposing(class)
+import Html.Attributes exposing (class)
+
 
 main : Html msg
 main =
-  -- div関数は引数としてリストを２つ取る。１つ目が属性のリスト、２つ目は小要素のリストとなる
-  div [class "header"] [ text "Hello, Elm HTML!"]
+    -- div関数は引数としてリストを２つ取る。１つ目が属性のリスト、２つ目は小要素のリストとなる
+    div [ class "header" ] [ text "Hello, Elm HTML!" ]
