b6dfb8fe541df60398966a1426eb9c62a07bc3c0
kengo-k <kurobanekengo@gmail.com>
2022-12-25 22:34:06 +0900

    レコード型を使った関数の例 #6

--- 01-hello_elm/src/Main.elm
+++ 01-hello_elm/src/Main.elm
@@ -1,4 +1,4 @@
-module Main exposing (main, sum)
+module Main exposing (..)
 
 import Html exposing (text)
 
@@ -16,6 +16,37 @@ message =
 sum : Int -> Int -> Int
 sum begin end =
     List.foldl (+) 0 (List.range begin end)
+
+
+
+-- レコード型のサンプル
+-- type alias でレコード型に名前をつけることができる
+
+
+type alias Person =
+    { name : String
+    , age : Int
+    }
+
+
+
+-- レコードを引数に取る関数のサンプル
+-- レコードの値を更新した新しいレコードを返す
+
+
+haveBirthday : Person -> Person
+haveBirthday person =
+    { name = person.name, age = person.age + 1 }
+
+
+
+-- 上記の新しいレコードを返す関数をより簡単に記述する例
+-- 更新したい値だけを明示してそれ以外の項目は既存の値をそのまま使うことができる
+
+
+haveBirthday2 : Person -> Person
+haveBirthday2 person =
+    { person | age = person.age + 1 }
 
 
 main : Html.Html msg
--- 01-hello_elm/tests/TestMain.elm
+++ 01-hello_elm/tests/TestMain.elm
@@ -7,9 +7,17 @@ import Test exposing (..)
 
 suite : Test
 suite =
-    describe "Example Test Suite"
-        [ test "should equal" <|
+    describe "tests"
+        [ test "should sum 1..10 equals 55" <|
             \() ->
                 sum 1 10
                     |> Expect.equal 55
+        , test "should haveBirthday add age + 1" <|
+            \() ->
+                haveBirthday { name = "Yamada", age = 25 }
+                    |> Expect.equal { name = "Yamada", age = 26 }
+        , test "should haveBirthday2 add age + 1" <|
+            \() ->
+                haveBirthday { name = "Tanaka", age = 32 }
+                    |> Expect.equal { name = "Tanaka", age = 33 }
         ]
